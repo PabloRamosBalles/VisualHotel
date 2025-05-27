c1433d837681130774653b4bfde4d641891be9a4
pabloramosballes <pablo.ramos.balles@gmail.com>
2025-05-27 12:08:40 +0200

    Create-views

--- backend/applications/reserve/views.py
+++ backend/applications/reserve/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .models import Reserve
+from .serializers import ReserveSerializer
+
+class ReserveViewSet(viewsets.ModelViewSet):
+    queryset = Reserve.objects.all()
+    serializer_class = ReserveSerializer
--- backend/applications/room/views.py
+++ backend/applications/room/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .models import Room
+from .serializers import RoomSerializer
+
+class RoomViewSet(viewsets.ModelViewSet):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
