c6e4ed4a237c44733135c79fe19050c258d5702d
pabloramosballes <pablo.ramos.balles@gmail.com>
2025-05-29 15:31:02 +0200

    Create admin site

--- backend/applications/customer/admin.py
+++ backend/applications/customer/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from applications.customer.models import Customer
 
 # Register your models here.
+admin.site.register(Customer)
--- backend/applications/reserve/admin.py
+++ backend/applications/reserve/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from applications.reserve.models import Reserve
 # Register your models here.
+admin.site.register(Reserve)
