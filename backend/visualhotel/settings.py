a101dfc57b2e48da593b2ec1d9f8ac3cc8ff3f79
pabloramosballes <pablo.ramos.balles@gmail.com>
2025-06-10 16:14:30 +0200

    allowed-hosts-fix

--- backend/visualhotel/settings.py
+++ backend/visualhotel/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['visualhotel.onrender.com', 'localhost']
 
 
 # Application definition
