0c64fa613a8e2c017c9a741f7a85a56d969653e7
pabloramosballes <pablo.ramos.balles@gmail.com>
2025-06-10 13:38:37 +0200

    deploy-settings

--- backend/visualhotel/settings.py
+++ backend/visualhotel/settings.py
@@ -60,7 +60,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ROOT_URLCONF = 'visualhotel.urls'
 
